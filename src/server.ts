60d6a98e2d488786dd762e7e85c7c27c44917453
Robertoo.Felipe <feliperoberto092@gmail.com>
2022-09-20 00:19:42 -0300

    Add: :construction: starting create a route page

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import express from "express";
+//import route from './routes';
 
 const app = express();
 const porta = process.env.PORT || 3000;
+
+app.use(express.json())
+//app.use(route)
 
 app.listen(porta, () => {
     console.log(`App is running in port ${porta}`)
