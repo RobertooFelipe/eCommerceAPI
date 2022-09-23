9ad19d3618bfb1ad31892c0794953f6b09d5dd5c
Robertoo.Felipe <feliperoberto092@gmail.com>
2022-09-22 23:58:18 -0300

    Add: :arrow_down: import methods to route file

--- src/routes/item.routes.ts
+++ src/routes/item.routes.ts
@@ -1,12 +1,20 @@
-import { Router  } from "express";
+import { Router } from "express";
 import { CreateItem } from "../controller/createItems.controller";
+import { FindItem } from "../controller/findItem.controller";
+import { ListAllItems } from "../controller/listItems.controller";
+import { AlterItem } from "../controller/alterItem.controller";
+import { DeleteItem } from "../controller/deleteItem.controller";
 
 const createItem = new CreateItem;
+const findItem = new FindItem;
+const listAllItems = new ListAllItems;
+const alterItem = new AlterItem;
+const deleteItem = new DeleteItem;
 
 export const route = Router()
 
-route.get('./listItems',);
-route.get('./findItem/:id',);
-route.post('/createItem',createItem.Create);
-route.put('./alterItem/:id',);
-route.delete('./deleteItem/:id',);
+route.get('/listItems',listAllItems.listAll);
+route.get('/findItem/:id',findItem.list);
+route.post('/createItem',createItem.create);
+route.put('/alterItem/:id',alterItem.alter);
+route.delete('/deleteItem/:id',deleteItem.remove);
